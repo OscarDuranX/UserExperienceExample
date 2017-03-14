678dfa4278ca0a68b46b48a6d16d2c4e84509562
Oscar Duran <xoscarduranx@gmail.com>
2017-03-14 17:41:06 +0100

    Initial version

---
+++ resources/assets/js/components/auth/Registerform.vue
@@ -0,0 +1,73 @@
+<template>
+    <form method="post" @submit.prevent="submit">
+        <!--<input type="hidden" name="_token" value="{{ csrf_token() }}">-->
+        <div class="form-group has-feedback">
+            <input type="text" class="form-control" placeholder="Your name here" :name="name" value=""/>
+            <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+
+        <div class="form-group has-feedback">
+            <input type="email" class="form-control" placeholder="" :name="email" value=""/>
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="" :name="password"/>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Password_here" :name="password_confirmation"/>
+            <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+        </div>
+        <div class="row">
+            <div class="col-xs-1">
+                <label>
+                    <div class="checkbox_register icheck">
+                        <label>
+                            <input type="checkbox" :name="terms">
+                        </label>
+                    </div>
+                </label>
+            </div><!-- /.col -->
+            <div class="col-xs-6">
+                <div class="form-group">
+                    <button type="button" class="btn btn-block btn-flat" data-toggle="modal" data-target="#termsModal">Termsbutton</button>
+                </div>
+            </div><!-- /.col -->
+            <div class="col-xs-4 col-xs-push-1">
+                <button type="submit" class="btn btn-primary btn-block btn-flat">Submit</button>
+            </div><!-- /.col -->
+        </div>
+    </form>
+</template>
+<script>
+export default {
+  mounted() {
+    console.log('Component Register Form mounted.')
+  },
+  date: function (){
+    return {
+      name: '',
+      email: '',
+      password: '',
+      password_confirmation: '',
+      terms: false,
+    }
+  },
+  methods: {
+    submit() {
+      //Client http
+        //Promise
+      axios.post('/register',{
+          name: 'Fred',
+          email: 'prova@prova.com'
+        })
+      .then(function(response){
+        console.log(response)
+      })
+      .catch(function(error){
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
